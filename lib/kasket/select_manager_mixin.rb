9401ed3a3491fad9d810e0ab795d3187caec5b0a
Michael Grosser <michael@grosser.it>
2015-09-14 15:37:36 -0700

    do not query db for where(id: [1]) style queries, they get created when using includes with just 1 element

--- lib/kasket/select_manager_mixin.rb
+++ lib/kasket/select_manager_mixin.rb
@@ -9,16 +9,18 @@ module Kasket
 
       return nil if query.nil? || query == :unsupported
       return nil if query[:attributes].blank?
+      return nil if query[:limit] > 1 if query[:limit]
 
       query[:index] = query[:attributes].map(&:first)
-
-      if query[:limit]
-        return nil if query[:limit] > 1
-        # return nil if !query[:index].include?(:id)
-      end
-
-      if query[:index].size > 1 && query[:attributes].any? { |attribute, value| value.is_a?(Array) }
-        return nil
+      if query[:index].size > 1
+        attributes = query[:attributes]
+        attributes.each do |attribute_and_value|
+          value = attribute_and_value[1]
+          if value.is_a?(Array)
+            return nil if value.size != 1
+            attribute_and_value[1] = value.first
+          end
+        end
       end
 
       query[:key] = klass.kasket_key_for(query[:attributes])
