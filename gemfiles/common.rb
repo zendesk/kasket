7679901eda30330f677ce481972ccef2bd6226cd
Pierre Schambacher <pschambacher@zendesk.com>
2016-02-12 11:12:09 +0000

    Modernize gemfiles

--- gemfiles/common.rb
+++ gemfiles/common.rb
@@ -1,6 +1,8 @@
-source "https://rubygems.org"
+source 'https://rubygems.org'
 
-gem "mysql2", "~> 0.3.0", platforms: :ruby
-gem "activerecord-jdbcmysql-adapter", "~> 1.2", platforms: :jruby
-gem 'mocha', git: 'https://github.com/zendesk/mocha.git', branch: "eac/alias_method_fix" # https://github.com/freerange/mocha/pull/202
+gem 'mysql2', '~> 0.3.0', platforms: :ruby
+gem 'activerecord-jdbcmysql-adapter', '~> 1.2', platforms: :jruby
+gem 'mocha', git: 'https://github.com/zendesk/mocha.git', branch: 'eac/alias_method_fix' # https://github.com/freerange/mocha/pull/202
 gem 'byebug', platforms: :ruby
+
+gemspec path: Bundler.root.sub('/gemfiles', '')
