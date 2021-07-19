76649bf529a9965bcc59e810c00f0b6c1a7fc56b
Steven Bloch <steven.bloch@zendesk.com>
2021-07-19 11:41:49 -0700

    v4.8.1

--- lib/kasket/version.rb
+++ lib/kasket/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Kasket
-  VERSION = '4.8.0'
+  VERSION = '4.8.1'
   class Version
     MAJOR = Kasket::VERSION.split('.')[0]
     MINOR = Kasket::VERSION.split('.')[1]
