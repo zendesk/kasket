b8a01098fbda314b6bd4ce69c14d1360b7a15651
Ryan Davis <ryand-ruby@zenspider.com>
2021-12-22 12:55:21 -0800

    Pull up mysql2 dependency to common.rb and fix specifier.

--- gemfiles/common.rb
+++ gemfiles/common.rb
@@ -2,6 +2,8 @@
 source 'https://rubygems.org'
 
 gem 'activerecord-jdbcmysql-adapter', '~> 1.2', platforms: :jruby
+gem 'mysql2', '~> 0.4.0', platforms: :ruby
+
 gem 'byebug', platforms: :ruby
 gem 'rubocop', '~> 0.89.1', platforms: :ruby
 
