288b1c7cc4ed6d00a422bc44b181347e7dd4a092
AlisonAcuna <alison.acuna@zendesk.com>
2022-06-09 14:52:20 -0700

    v4.10.1

--- lib/kasket/version.rb
+++ lib/kasket/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Kasket
-  VERSION = '4.10.0'
+  VERSION = '4.10.1'
   class Version
     MAJOR = Kasket::VERSION.split('.')[0]
     MINOR = Kasket::VERSION.split('.')[1]
