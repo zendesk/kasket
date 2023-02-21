1dd4ca43289558a01d3783017127117026fecbb6
Tommaso Pavese <tommaso.pavese@zendesk.com>
2023-02-21 17:21:11 +0100

    Add configuration option for an events_callback

--- lib/kasket.rb
+++ lib/kasket.rb
@@ -18,17 +18,30 @@ module Kasket
   CONFIGURATION = { # rubocop:disable Style/MutableConstant
     max_collection_size: 100,
     write_through: false,
-    default_expires_in: nil
+    default_expires_in: nil,
+    events_callback: nil,
   }
 
   module_function
 
+  # Configure Kasket.
+  #
+  # @param [Hash] options the configuration options for Kasket.
+  # @option options [Integer] :max_collection_size max size limit for a cacheable
+  #   collection of records.
+  # @option options [Boolean] :write_through
+  # @option options [Integer, nil] :default_expires_in the cache TTL.
+  # @option options [#call] :events_callback a callable object used to instrument
+  #   Kasket operations. It is invoked with two arguments: the name of the event,
+  #   as a String, and the Klass of the ActiveRecord model the event is about.
+  #
   def setup(options = {})
     return if ActiveRecord::Base.respond_to?(:has_kasket)
 
     CONFIGURATION[:max_collection_size] = options[:max_collection_size] if options[:max_collection_size]
     CONFIGURATION[:write_through]       = options[:write_through]       if options[:write_through]
     CONFIGURATION[:default_expires_in]  = options[:default_expires_in]  if options[:default_expires_in]
+    CONFIGURATION[:events_callback]     = options[:events_callback]     if options[:events_callback]
 
     ActiveRecord::Base.extend(Kasket::ConfigurationMixin)
     ActiveRecord::Relation.include(Kasket::RelationMixin)
