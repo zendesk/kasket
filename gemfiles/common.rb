c02f25268294297d1905753a5042cd8e6820054f
Ryan Davis <ryand-ruby@zenspider.com>
2023-07-20 14:46:12 -0700

    Pull mysql up to common. Version shouldn't matter

--- gemfiles/common.rb
+++ gemfiles/common.rb
@@ -2,17 +2,17 @@
 source 'https://rubygems.org'
 
 # dev dependencies:
-gem "pry",         "~> 0.13.1"
-gem "byebug",      "~> 11.1"
-gem "pry-byebug",  "= 3.9.0"
-gem "bump",        "~> 0.10"
-gem "minitest",    "~> 5.16.2" # temporary evil until https://github.com/zendesk/kasket/pull/82
-gem "minitest-rg", "~> 5.2"
-gem "mocha",       "~> 1.13"
-gem "rake",        "~> 13"
-gem "timecop",     "~> 0.9"
-gem "rubocop",     "~> 1.5.0"
-gem "rubocop-performance", "~> 1.10.2"
-gem "rubocop-rubycw",      "~> 0.1.6"
+gem "pry"
+gem "byebug"
+gem "pry-byebug"
+gem "bump"
+gem "minitest"
+gem "minitest-rg"
+gem "mocha"
+gem "rake"
+gem "timecop"
+gem "rubocop"
+gem "rubocop-performance"
+gem "rubocop-rubycw"
 
 gemspec path: Bundler.root.sub('/gemfiles', '')
