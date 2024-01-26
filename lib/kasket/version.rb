96163c4801e684aab8fcc4a13266edf00656cd91
John Meade <john.meade@zendesk.com>
2024-01-26 09:58:50 +0000

    v4.14.0

--- lib/kasket/version.rb
+++ lib/kasket/version.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 module Kasket
-  VERSION = '4.13.0'
+  VERSION = '4.14.0'
   class Version
     MAJOR = Kasket::VERSION.split('.')[0]
     MINOR = Kasket::VERSION.split('.')[1]
